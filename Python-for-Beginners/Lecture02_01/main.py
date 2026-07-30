7f7213edf31f2c92da16b23af17be83f1e7336c4
BYSooooo <avera0128@gmail.com>
2026-07-30 23:43:36 +0900

    [260730]Nomad Coders Lecture

---
+++ Python-for-Beginners/Lecture02_01/main.py
@@ -0,0 +1,29 @@
+a = 2
+b = 3
+c = a + b
+print(c)
+## C = 5
+
+a = 2
+b = 3
+c = a + b
+c = 1
+print(c)
+## c = 1
+
+a = 2
+b = 3
+c = a + b
+a = 1
+b = 1
+print(c)
+# c = 5
+
+## Naming Rule
+my age = 88 #(X)
+my_age = 88 #(O)
+myAge = 88 #(O)
+654my_age = 88 #(X)
+my_age1 = 88 #(O)
+
+ 
