60b6dcf7419403d54f8d01bd340b89e5c534c6b4
BYSooooo <avera0128@gmail.com>
2026-08-11 22:33:53 +0900

    [260811]Nomad Coders Lecture

---
+++ Python-for-Beginners/Lecture03_00/main.py
@@ -0,0 +1,14 @@
+## If (Condition)
+
+# Basic
+# if condition:
+#    "write the code to run" 
+
+if 10 > 5:  # 1. 10 > 5 = true
+    print("Correct!") #2. print()
+
+if 10 < 5: # 1. 10 < 5 = false
+    print("Correct!")   #2. X
+
+
+
