072bb8f7527603d970d66577c46219f2f217fcba
BYSooooo <avera0128@gmail.com>
2026-08-20 23:48:59 +0900

    [260820]Nomad Coders Lecture

---
+++ Python-for-Beginners/Lecture03_05/main.py
@@ -0,0 +1,14 @@
+## While
+
+# Case 1. - Basic structure
+# Infinity loop until using all memory.
+# while True: 
+#     print("Hi i'm True")
+
+# Case 2. - Add condition
+distance = 0
+
+# Called until distance is 19
+while distance < 20:
+    print("I'm running:",distance, "km")
+    distance = distance + 1
