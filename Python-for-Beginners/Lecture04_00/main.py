47c26b2e909a98561867c687c0dce69cf0d2060b
BYSooooo <avera0128@gmail.com>
2026-08-25 22:46:49 +0900

    [260825]Nomad Coders Lecture

---
+++ Python-for-Beginners/Lecture04_00/main.py
@@ -0,0 +1,14 @@
+## Methods
+
+name = 'nico'
+
+# print() = Function
+# .upper() = Method (Change Data to Upper Case)
+print(name.upper()) # NICO
+
+#.capitalize() = Method (Change First Character to Upper Case)
+print(name.capitalize()) # Nico
+
+# .endswidth() = Method (return boolean value by checking data's last character equals argument value )
+print(name.endswith('o')) # True
+
