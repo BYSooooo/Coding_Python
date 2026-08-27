8c9f48998b0703b659e0a1f3cadf756f409c8284
BYSooooo <avera0128@gmail.com>
2026-08-27 23:43:09 +0900

    [260827]Nomad Coders Lecture

---
+++ Python-for-Beginners/Lecture04_01/main.py
@@ -0,0 +1,35 @@
+## Lists
+
+# List
+day_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+
+# Check Method of List by Typing '.' after List
+#day_of_week.
+
+# Count Method
+wed_count = day_of_week.count("Wed")
+print(wed_count) # 1
+
+# Clear Method
+day_of_week_1 = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+day_of_week_1.clear()
+print(day_of_week_1) # []
+
+# Reverse
+day_of_week_2 = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+day_of_week_2.reverse()
+print(day_of_week_2) #['Fri', 'Thu', 'Wed', 'Tue', 'Mon']
+
+# Append
+day_of_week_3 = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+day_of_week_3.append("Sat")
+print(day_of_week_3)
+day_of_week_3.append("Sun")
+print(day_of_week_3)
+
+# Select Element using index
+day_of_week_4 = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+third_item = day_of_week_4[2]
+print(third_item)
+
+
