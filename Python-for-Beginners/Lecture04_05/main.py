1805deeed28582e99c9efa567acbe0759ca41eea
BYSooooo <avera0128@gmail.com>
2026-09-01 23:55:22 +0900

    [260901]Nomad Coders Lecture

---
+++ Python-for-Beginners/Lecture04_05/main.py
@@ -0,0 +1,15 @@
+## For Loop
+
+# Base Tuple
+websites = (
+    "google.com",
+    "airbnb.com",
+    "twitter.com",
+    "facebook.com"
+)
+
+# Loop Base
+# each = each element of websites 
+for each in websites:
+    # for use element, call each in loop
+    print(each)
