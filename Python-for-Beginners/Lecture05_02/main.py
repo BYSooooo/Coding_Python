4827ab8cec5e482fe6ecfa507dbda8604317169c
BYSooooo <avera0128@gmail.com>
2026-09-13 00:10:18 +0900

    [260913]Nomad Coders Lecture

--- Python-for-Beginners/Lecture05_02/main.py
+++ Python-for-Beginners/Lecture05_02/main.py
@@ -1,11 +1,20 @@
 ## Classes
 
-# Declar of Class
+# Define Class
 class Puppy:
     pass
 
 ruffus = Puppy()
 print(ruffus)
+
+# Add Method Test
+class Puppy1:
+    def __init__():
+        print("Puppy is born!")
+
+ruffus1 = Puppy1()
+# Puppy1 has Error because has no argument.
+print(ruffus1)
 
 # Add Method
 class Puppy2:
@@ -16,3 +25,19 @@ class Puppy2:
         print("Puppy is born!")
 
 ruffus2 = Puppy2()
+
+# Define Argument
+class Puppy3:
+    def __init__(self):
+        # Set a variable to argument
+        self.name = "Ruffus"
+        self.age = 0.1
+        self.bread = "Beagle"
+
+ruffus3 = Puppy3()
+
+# check argument variable
+print(ruffus3.name)
+print(ruffus3.age)
+print(ruffus3.bread)
+
