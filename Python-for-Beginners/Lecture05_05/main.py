af2a19640d0fdc2de0331db1144dc5e2f0195985
BYSooooo <avera0128@gmail.com>
2026-09-20 00:34:21 +0900

    [260920]Nomad Coders Lecture

--- Python-for-Beginners/Lecture05_05/main.py
+++ Python-for-Beginners/Lecture05_05/main.py
@@ -15,6 +15,7 @@ class GuardDog(Dog):
     def rrrr(self):
         print("Stay away!")
 
+# Error - no Argument about 'age'
 ruffus = Puppy(
     name="Ruffus",
     breed="Beagle"
@@ -24,3 +25,30 @@ bibi = GuardDog(
     name="Bibi",
     breed="Dalmatian"
 )
+
+# Add Control of age Argument
+class Puppy2(Dog):
+    def __init__(self, name, breed):
+        # super() = Reference of class Dog
+        super().__init__(
+            name,
+            breed,
+            0.1
+        )
+
+    def woof_woof(self):
+        print("Woof Woof")
+
+class GuardDog2(Dog):
+    def __init__(self, name, breed):
+        super().__init__(name, breed, 5)
+
+    def rrrr(self):
+        print("Stay away!")
+
+# Don't need to add age argument because it is handled in each Class
+ruffus2 = Puppy2(name="Ruffus", breed = "Beagle")
+bibi2 = GuardDog2(name="Bibi", breed="Dalmatian")
+
+print(ruffus2.age)  # 0.1
+print(bibi2.age)    # 5
