345a72979b213dc79d6627578376abf6d1f95dd9
BYSooooo <avera0128@gmail.com>
2026-09-25 01:01:24 +0900

    [260925]Nomad Coders Lecture

--- Python-for-Beginners/Lecture06_02/main.py
+++ Python-for-Beginners/Lecture06_02/main.py
@@ -6,15 +6,22 @@ from bs4 import BeautifulSoup
 # URL of Site for scrapping
 url = 'https://weworkremotely.com/categories/remote-full-stack-programming-jobs'
 
+# Test Connection
 response = requests.get(url)
+print(response.content)
 
+# use BeautifulSoup
 soup = BeautifulSoup(response.content, "html.parser")
 
-jobs = soup.find("section",class_="jobs").find_all("li")[0:-1]
+jobs = soup.find("section", class_="jobs")
+list = jobs.find_all("li", class_="new-listing-container")
 
-for job in jobs:
-    title = job.find('h3','new-listing__header__title').text
-    companies = job.find_all("p","new-listing__categories__category")
-    for idx in range(len(companies)):
-        company_within = companies[idx].text
-        print(title,"-----------",company_within)
+for job in list:
+    title = job.find('h3', class_="new-listing__header__title").text
+    shape = ""
+    categories = job.find_all('p', class_="new-listing__categories__category")
+    for idx in range(len(categories)):
+        within = categories[idx].text
+        shape = shape + within
+
+    print(title, "------", shape)
