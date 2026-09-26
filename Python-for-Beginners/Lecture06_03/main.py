89bb6f051d1ad1a5ee6ec09e7c7d5404eceabe08
BYSooooo <avera0128@gmail.com>
2026-09-27 00:27:53 +0900

    [260927]Nomad Coders Lecture

--- Python-for-Beginners/Lecture06_03/main.py
+++ Python-for-Beginners/Lecture06_03/main.py
@@ -8,6 +8,15 @@ a[0:5] # 1,2,3,4,5
 a[1:] # 2,3,4,5,6,7,8,9
 a[1:-1] # 2,3,4,5,6,7,8
 
+# intro2 - short cut
+letters = ["a", "b", "c"]
+
+letters[1] # "a"
+
+q,w,e = letters # q = "a", w = "b", e = "c"
+print(q,w,e)
+
+
 #Basic Structure
 import requests
 from bs4 import BeautifulSoup
@@ -26,6 +35,16 @@ list = jobs.find_all("li", class_="new-listing-container")
 
 for job in list:
     title = job.find('h3', class_="new-listing__header__title").text
+    # Get a Company Information
     company = job.find('p', class_="new-listing__company-name").text
-
-    print(f"{title} ----- {company}")
+    # Get a Company Headquater Information
+    headquater = job.find('p', class_="new-listing__company-headquarters")
+    
+    # set a string 
+    headquater_text = ""
+    if headquater :
+        headquater_text = f"({headquater.text})"
+    else:
+        headquater_text = ""
+    
+    print(f"{title} - {company}{headquater_text}")
